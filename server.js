d50315e209c19179de4ef19bfb1a5acaa72c33db
Kauamoraes <kauamoraesvieira2021@gmail.com>
2026-02-06 16:56:31 -0300

    cors

--- server.js
+++ server.js
@@ -16,7 +16,9 @@ import authRoutes from "./src/routes/auth.routes.js";
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(cors({
+  origin: '*'
+}));
 
 app.use(express.json());
 
